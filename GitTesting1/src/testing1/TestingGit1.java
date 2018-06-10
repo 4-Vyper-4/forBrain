0695d5d97e79352187cd005ed8d1598e70bffcd6
BrianGuadron <brianguadron@gmail.com>
2018-06-10 18:16:34 -0400

    create project

---
+++ GitTesting1/src/testing1/TestingGit1.java
@@ -0,0 +1,17 @@
+package testing1;
+
+public class TestingGit1 {
+	
+	public String toString() {
+		return "Java is awesome";
+	}
+
+	public static void main(String[] args) {
+		
+		TestingGit1 t = new TestingGit1();
+		System.out.println(t.toString());
+		
+
+	}
+
+}
