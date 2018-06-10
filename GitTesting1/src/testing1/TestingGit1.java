108fcfc1eebf9c980b19c49f052b3a248b9c3574
4-Vyper-4 <charles.s.cybertek@gmail.com>
2018-06-10 18:28:16 -0400

    add pronounciation

--- GitTesting1/src/testing1/TestingGit1.java
+++ GitTesting1/src/testing1/TestingGit1.java
@@ -1,17 +1,17 @@
 package testing1;
 
 public class TestingGit1 {
-	
+
 	public String toString() {
 		return "Java is awesome";
 	}
 
 	public static void main(String[] args) {
-		
+
 		TestingGit1 t = new TestingGit1();
 		System.out.println(t.toString());
-		
-System.out.println("working");
-	}
 
+		System.out.println("working");
+		System.out.println("Am - Big - You - Us");
+	}
 }
