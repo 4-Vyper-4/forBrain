7ba0629b76e130b634eacb094a363f26c7aad8b4
BrianGuadron <brianguadron@gmail.com>
2018-06-11 11:20:05 -0400

    add method that reverse a String

--- GitTesting1/src/testing1/TestingGit1.java
+++ GitTesting1/src/testing1/TestingGit1.java
@@ -5,6 +5,12 @@ public class TestingGit1 {
 	public String toString() {
 		return "Java is awesome";
 	}
+	
+	 public static String reverse(String s) {
+	      if (s.length()>0)
+	      return s.substring(s.length()-1)+reverse(s.substring(0, s.length()-1));
+	      return "";
+	    }
 
 	public static void main(String[] args) {
 
@@ -26,5 +32,7 @@ public class TestingGit1 {
 		System.out.println("working");
 		System.out.println("Am - Big - You - Us");
 		System.out.println("stop with the writing, start with the practice");
+		System.out.println(t.reverse("Jonny stop cd cd cd all the time in Git"));
+		System.out.println("Thats my way of learning SIR");
 	}
 }
